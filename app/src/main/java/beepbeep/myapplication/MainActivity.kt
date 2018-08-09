0e6185e42dc5a352451e57b32041c8ec659e0e61
Tan Jun Rong <tanjunrong@gmail.com>
2018-08-09 09:38:36 +0900

    refactoring

--- app/src/main/java/beepbeep/myapplication/MainActivity.kt
+++ app/src/main/java/beepbeep/myapplication/MainActivity.kt
@@ -10,30 +10,35 @@ data class DataModel(val title: String, val desc: String)
 
 class MainActivity : AppCompatActivity() {
 
+    val CONSTANT = 999
+
+    val dataArray: List<DataModel> = listOf(
+            DataModel("Dinosaur", "this is a T-Rex"),
+            DataModel("Fruit", "papaya avocado mango"),
+            DataModel("Gadget", "mouse keyboard")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dataArray: List<DataModel> = listOf(
-                DataModel("Dinosaur", "this is a T-Rex"),
-                DataModel("Fruit", "papaya avocado mango"),
-                DataModel("Gadget", "mouse keyboard")
-        )
-
-        val CONSTANT = 999
         dataArray.forEachIndexed { index, dataModel ->
-            val row = layoutInflater.inflate(R.layout.row, container, false)
-            row.id = index + CONSTANT
-            row.titleTextView.text = dataModel.title
+            val row = layoutInflater.inflate(R.layout.row, container, false).apply {
+                id = index + CONSTANT
+                titleTextView.text = dataModel.title
+            }
+
             container.addView(row)
 
-            val set = ConstraintSet()
-            set.clone(container)
-            if (index == 0) {
-                set.connect(row.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP)
-            } else {
-                set.connect(row.id, ConstraintSet.TOP, row.id - 1, ConstraintSet.BOTTOM)
+            val set = ConstraintSet().apply {
+                clone(container)
+                if (index == 0) {
+                    connect(row.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP)
+                } else {
+                    connect(row.id, ConstraintSet.TOP, row.id - 1, ConstraintSet.BOTTOM)
+                }
             }
+
             set.applyTo(container)
         }
     }
