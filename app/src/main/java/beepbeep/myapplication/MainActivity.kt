0ebcae65e3e8f90fb25f0256895e88fb75628653
Tan Jun Rong <tanjunrong@gmail.com>
2018-08-09 09:32:18 +0900

    using loop

--- app/src/main/java/beepbeep/myapplication/MainActivity.kt
+++ app/src/main/java/beepbeep/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package beepbeep.myapplication
 import android.os.Bundle
 import android.support.constraint.ConstraintSet
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.row.view.*
 
@@ -18,26 +19,53 @@ class MainActivity : AppCompatActivity() {
                 DataModel("Gadget", "mouse keyboard")
         )
 
-        val row = layoutInflater.inflate(R.layout.row, container, false)
-        row.id = 1
-        val row2 = layoutInflater.inflate(R.layout.row, container, false)
-        row2.id = 2
-        row2.titleTextView.text = "LALAL"
+        val CONSTANT = 999
+        dataArray.forEachIndexed { index, dataModel ->
+            val row = layoutInflater.inflate(R.layout.row, container, false)
+            row.id = index + CONSTANT
+            row.titleTextView.text = dataModel.title
+            container.addView(row)
+            Log.d("ddw", "${index} here")
 
-        val content = layoutInflater.inflate(R.layout.content, container, false)
-        content.id = 3
+            val set = ConstraintSet()
+            set.clone(container)
+            if (index == 0) {
+                Log.d("ddw", "${index} here A")
+                set.connect(row.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP)
+            } else {
+                Log.d("ddw", "${index} here B")
+                set.connect(row.id, ConstraintSet.TOP, row.id - 1, ConstraintSet.BOTTOM)
+            }
+            set.applyTo(container)
+        }
+//        val row = layoutInflater.inflate(R.layout.row, container, false)
+//        row.id = 1
+//        container.addView(row)
+////        if (index == 0) {
+//        val set = ConstraintSet()
+//        set.clone(container)
+//            set.connect(1, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP)
+//        set.applyTo(container)
+//        val row = layoutInflater.inflate(R.layout.row, container, false)
+//        row.id = 1
+//        val row2 = layoutInflater.inflate(R.layout.row, container, false)
+//        row2.id = 2
+//        row2.titleTextView.text = "LALAL"
 
-        container.addView(row)
-        container.addView(row2)
-        container.addView(content)
+//        val content = layoutInflater.inflate(R.layout.content, container, false)
+//        content.id = 3
 
-        val set = ConstraintSet()
-        set.clone(container)
-        set.connect(1, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP)
-        set.connect(2, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM)
-        set.connect(3, ConstraintSet.TOP, 1, ConstraintSet.BOTTOM)
-        set.connect(3, ConstraintSet.BOTTOM, 2, ConstraintSet.TOP)
-        set.applyTo(container)
+//        container.addView(row)
+//        container.addView(row2)
+//        container.addView(content)
+
+//        val set = ConstraintSet()
+//        set.clone(container)
+//        set.connect(1, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP)
+//        set.connect(2, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM)
+//        set.connect(3, ConstraintSet.TOP, 1, ConstraintSet.BOTTOM)
+//        set.connect(3, ConstraintSet.BOTTOM, 2, ConstraintSet.TOP)
+//        set.applyTo(container)
     }
 
     data class DataModel(val title: String, val desc: String)
