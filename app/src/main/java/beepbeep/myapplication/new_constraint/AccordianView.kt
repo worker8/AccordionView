2ca877d83c483fcc84709d7714e0c5cc877de816
Tan Jun Rong <tanjunrong@gmail.com>
2018-08-11 16:03:31 +0900

    clear unused code

--- app/src/main/java/beepbeep/myapplication/new_constraint/AccordianView.kt
+++ app/src/main/java/beepbeep/myapplication/new_constraint/AccordianView.kt
@@ -38,13 +38,6 @@ class AccordianView @JvmOverloads constructor(context: Context, attrs: Attribute
         renderContent()
         addAllViews()
         applyConstraint()
-        // selected position
-        // make content view
-
-
-//        renderTopSection()
-//        renderBottomSection()
-//        renderContentSection()
     }
 
     private fun addAllViews() {
@@ -69,7 +62,6 @@ class AccordianView @JvmOverloads constructor(context: Context, attrs: Attribute
                     }
                     row.setOnClickListener2 {
                         selectedPosition = index
-//                        applyConstraint()
                     }
                     val arrowDirection = if (selectedPosition == index) {
                         AccordianAdapter.ArrowDirection.NONE
@@ -124,132 +116,6 @@ class AccordianView @JvmOverloads constructor(context: Context, attrs: Attribute
         }
         TransitionManager.beginDelayedTransition(this)
         set.applyTo(this)
-//
-//        val set2 = ConstraintSet()
-//        set2.clone(this)
-//        contentViewHolder?.let {
-//            val content = it.itemView
-//            val positionAboveContent = selectedPosition
-//            val positionBelowContent = selectedPosition + 1
-//
-//            val rowIdAboveContent = titleViewHolderArray[positionAboveContent].itemView.id
-//            val rowIdBelowContent = if (selectedPosition == titleViewHolderArray.size - 1) {
-//                ConstraintSet.PARENT_ID
-//            } else {
-//                titleViewHolderArray[positionBelowContent].itemView.id
-//            }
-//
-//            set2.connect(content.id, ConstraintSet.TOP, rowIdAboveContent, ConstraintSet.BOTTOM)
-//            set2.connect(
-//                    content.id, ConstraintSet.BOTTOM, rowIdBelowContent, if (selectedPosition == titleViewHolderArray.size - 1) {
-//                ConstraintSet.BOTTOM // align to parent's bottom if it is the last one
-//            } else {
-//                ConstraintSet.TOP
-//            }
-//            )
-//        }
-//        TransitionManager.beginDelayedTransition(this)
-//        set2.applyTo(this)
-    }
-
-    private fun renderTopSection() {
-        adapter?.let { _adapter ->
-            for (index in 0..selectedPosition) {
-                val rowViewHolder = _adapter.onCreateViewHolderForTitle(this)
-                val row = rowViewHolder.itemView.apply {
-                    id = index + ID_OFFSET
-                    tag = id.toString()
-                }
-                row.setOnClickListener2 {
-                    selectedPosition = index
-                    render()
-                }
-                addView(row)
-
-                val arrowDirection = if (selectedPosition == index) {
-                    AccordianAdapter.ArrowDirection.NONE
-                } else {
-                    AccordianAdapter.ArrowDirection.DOWN
-                }
-                _adapter.onBindViewForTitle(rowViewHolder, index, arrowDirection)
-
-                ConstraintSet().apply {
-                    clone(this@AccordianView) // it is important to clone from the parent, not from the empty ConstraintSet(), becareful!
-                    if (index == 0) {
-                        connect(row.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP)
-                    } else {
-                        connect(row.id, ConstraintSet.TOP, row.id - 1, ConstraintSet.BOTTOM)
-                    }
-                }.applyTo(this)
-            }
-        }
-    }
-
-    private fun renderBottomSection() {
-        adapter?.let { _adapter ->
-            for (reversedIndex in _adapter.getItemCount() - 1 downTo selectedPosition + 1) {
-                val rowViewHolder = _adapter.onCreateViewHolderForTitle(this)
-                val row = rowViewHolder.itemView.apply {
-                    id = reversedIndex + ID_OFFSET
-                    tag = id.toString()
-                }
-                row.setOnClickListener2 {
-                    selectedPosition = reversedIndex
-                    render()
-                }
-                this.addView(row)
-                val arrowDirection = if (selectedPosition == reversedIndex) {
-                    AccordianAdapter.ArrowDirection.NONE
-                } else {
-                    AccordianAdapter.ArrowDirection.UP
-                }
-                _adapter.onBindViewForTitle(rowViewHolder, reversedIndex, arrowDirection)
-                ConstraintSet().apply {
-                    clone(this@AccordianView) // it is important to clone from the parent, not from the empty ConstraintSet(), becareful!
-                    if (reversedIndex == _adapter.getItemCount() - 1) {
-                        connect(row.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM)
-                    } else {
-                        connect(row.id, ConstraintSet.BOTTOM, row.id + 1, ConstraintSet.TOP)
-                    }
-                }.applyTo(this)
-            }
-        }
-    }
-
-    private fun renderContentSection() {
-        adapter?.let { _adapter ->
-            val positionAboveContent = selectedPosition
-            val positionBelowContent = selectedPosition + 1
-
-            val rowIdAboveContent = positionAboveContent + ID_OFFSET
-            val rowIdBelowContent = if (selectedPosition == _adapter.getItemCount() - 1) {
-                ConstraintSet.PARENT_ID
-            } else {
-                positionBelowContent + ID_OFFSET
-            }
-
-            val contentViewHolder = _adapter.onCreateViewHolderForContent(this)
-
-            val content = contentViewHolder.itemView.apply {
-                id = View.generateViewId()
-                tag = id.toString()
-            }
-
-            addView(content)
-            _adapter.onBindViewForContent(contentViewHolder, selectedPosition)
-
-            ConstraintSet().apply {
-                clone(this@AccordianView) // it is important to clone from the parent, not from the empty ConstraintSet(), becareful!
-                connect(content.id, ConstraintSet.TOP, rowIdAboveContent, ConstraintSet.BOTTOM)
-                connect(
-                        content.id, ConstraintSet.BOTTOM, rowIdBelowContent, if (selectedPosition == _adapter.getItemCount() - 1) {
-                    ConstraintSet.BOTTOM // align to parent's bottom if it is the last one
-                } else {
-                    ConstraintSet.TOP
-                }
-                )
-            }.applyTo(this)
-        }
     }
 
     private fun View.setOnClickListener2(callback: (View) -> Unit) {
